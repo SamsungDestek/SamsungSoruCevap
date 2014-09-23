99132d16273768a528999f53246317a3891c5fa0
SamsungDestek <cevdetyener@outlook.com>
2014-09-24 01:00:16 +0300

    SoruCevaplari

--- SamsungSoruCevap/src/com/example/samsungsorucevap/Fragment/fragmentSoruCevaplari.java
+++ SamsungSoruCevap/src/com/example/samsungsorucevap/Fragment/fragmentSoruCevaplari.java
@@ -33,17 +33,14 @@ import java.util.ArrayList;
 
 public class fragmentSoruCevaplari extends Fragment {
 
-
 	ListView lvCevaplar;
 	ArrayList<Cevap> cevaplar;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.fragment_soru_cevaplari, container, false);
-		
-		
-		
+		return inflater.inflate(R.layout.fragment_soru_cevaplari, container,
+				false);
 	}
 
 	@Override
